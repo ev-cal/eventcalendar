fdf897aae8406890ef2a82634f1d6d9528893aad
anshidnm <anshidnm@gmail.com>
2022-03-12 18:02:33 +0530

    register log started

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from accounts import views
 from django.urls import path
 
 urlpatterns = [
- 
+ path('login',views.login,name="login"),
+ path('register',views.register,name="register"),
+ path('logout',views.logout,name="logout")
 ]
